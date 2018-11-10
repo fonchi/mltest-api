fcd75341025d9a82ec842379a8c99c21ee3161ff
Alfonso Vallone <alfonsovallone@gmail.com>
2018-11-10 16:39:34 -0300

    it changes port

--- src/main/java/com/exercise/mltest/MlTestApplication.java
+++ src/main/java/com/exercise/mltest/MlTestApplication.java
@@ -2,11 +2,17 @@ package com.exercise.mltest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MlTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MlTestApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "hello world!";
 	}
 }
